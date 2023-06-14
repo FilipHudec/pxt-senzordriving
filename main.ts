64d1b65bc0b88d4f8c2b8f25f711e74250fdc0d8
FilipHudec <115696159+FilipHudec@users.noreply.github.com>
2023-06-14 21:04:41 +0200

    dalsi verze

--- main.ts
+++ main.ts
@@ -1,5 +1,12 @@
 // na každé 5 měření dole => 1 měření US senzoru 50 - 10 Hz reakcemi za sekundu
 // nevolat digitalReadPin víckrát
+
+const _turn = 80
+interface senzors {
+    c: number;
+    l: number;
+    r: number;
+}
 radio.setGroup(112)
 
 let s1 = new Servo(PCAmotor.Servos.S1, 550, 2700, 1650, 10)
@@ -18,53 +25,85 @@ pins.setPull(pinR, PinPullMode.PullNone)
 s1.stop()
 
 function forward() {
-    PCAmotor.MotorRun(PCAmotor.Motors.M1, 100)
-    PCAmotor.MotorRun(PCAmotor.Motors.M4, -100)
+    PCAmotor.MotorRun(PCAmotor.Motors.M1, 128)
+    PCAmotor.MotorRun(PCAmotor.Motors.M4, -128)
 }
 
 function left(x: number) {
     PCAmotor.MotorRun(PCAmotor.Motors.M1, x)
-    PCAmotor.MotorRun(PCAmotor.Motors.M4, 0) // Adjust the right motor speed as needed
+    PCAmotor.MotorRun(PCAmotor.Motors.M4, 0) 
 }
 
 function right(x: number) {
-    PCAmotor.MotorRun(PCAmotor.Motors.M1, 0) // Adjust the left motor speed as needed
+    PCAmotor.MotorRun(PCAmotor.Motors.M1, 0)
     PCAmotor.MotorRun(PCAmotor.Motors.M4, -x)
 }
+
 function rotate(m1: number, m2: number) {
-    PCAmotor.MotorRun(PCAmotor.Motors.M1, m1) // Adjust the left motor speed as needed
+    PCAmotor.MotorRun(PCAmotor.Motors.M1, m1)
     PCAmotor.MotorRun(PCAmotor.Motors.M4, m2)
 }
 
-// fwd = 0; left = 1; r = 2
+
 let go = 0
+let paused = false
+
+function crossRotate (go: number) {
+    paused = true
+    PCAmotor.MotorStopAll()
+    if (go == 1) 
+    {
+        rotate(-108, -128)
+    }
+    else if (go == 2) 
+    {
+        rotate(128, 108)
+    }
+    else {
+        PCAmotor.MotorStopAll()
+        rotate(60, -60)
+    }
+    basic.pause(550)
+    paused = false
+}
 
 radio.onReceivedValue(function(name: string, value: number) {
-    if (name == "gocross") go = value
+    if (name == "gocross") 
+    {
+        go = value
+    }
+    if (name == "forcego") {
+        crossRotate(go)
+    }
 })
+
+function getSenzors () : senzors {
+    let c = (whiteLine ^ pins.digitalReadPin(pinC)) == 0 ? 0 : 1
+    let l = (whiteLine ^ pins.digitalReadPin(pinL)) == 0 ? 0 : 1
+    let r = (whiteLine ^ pins.digitalReadPin(pinR)) == 0 ? 0 : 1
+    return {c,l,r}
+}
+
 basic.forever(function () {
-    if (autoModeEnabled) {
-        let c = (whiteLine ^ pins.digitalReadPin(pinC)) == 0 ? 0 : 1
-        let l = (whiteLine ^ pins.digitalReadPin(pinL)) == 0 ? 0 : 1
-        let r = (whiteLine ^ pins.digitalReadPin(pinR)) == 0 ? 0 : 1
-    console.log([c,r,l])
+    if (paused) return
+    if (autoModeEnabled) { 
+        let s = getSenzors()
+        let c = s.c
+        let l = s.l
+        let r = s.r
+        console.log([c,r,l])
         if (!l && !r) {
             forward()
             console.log("center")
         } else if (!l) {
-            left(69)
+            left(_turn)
             console.log("left")
         } else if (!r) {
-            right(69)
+            right(_turn)
             console.log("right")
         } else if (l && r){
-            PCAmotor.MotorStopAll()
-            if (go == 1) rotate(-100, -128)
-            else if (go == 2) rotate(128, 100)
-            else rotate(50, -50)
-            basic.pause(550)
+            crossRotate(go)
         }
-        
 } 
 
 })
