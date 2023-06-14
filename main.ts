abfaa6cafbc22bd4400d50bf4fccce6ca9b4d874
FilipHudec <115696159+FilipHudec@users.noreply.github.com>
2023-06-14 21:11:21 +0200

    zmeny 3

--- main.ts
+++ main.ts
@@ -3,9 +3,9 @@
 
 const _turn = 80
 interface senzors {
-    c: number;
-    l: number;
-    r: number;
+    c: boolean;
+    l: boolean;
+    r: boolean;
 }
 radio.setGroup(112)
 
@@ -31,7 +31,7 @@ function forward() {
 
 function left(x: number) {
     PCAmotor.MotorRun(PCAmotor.Motors.M1, x)
-    PCAmotor.MotorRun(PCAmotor.Motors.M4, 0) 
+    PCAmotor.MotorRun(PCAmotor.Motors.M4, 0)
 }
 
 function right(x: number) {
@@ -44,22 +44,18 @@ function rotate(m1: number, m2: number) {
     PCAmotor.MotorRun(PCAmotor.Motors.M4, m2)
 }
 
-
+// 0 - fwd, 1 - left, 2 - right
 let go = 0
 let paused = false
 
-function crossRotate (go: number) {
+function crossRotate(go: number) {
     paused = true
     PCAmotor.MotorStopAll()
-    if (go == 1) 
-    {
+    if (go == 1) {
         rotate(-108, -128)
-    }
-    else if (go == 2) 
-    {
+    } else if (go == 2) {
         rotate(128, 108)
-    }
-    else {
+    } else {
         PCAmotor.MotorStopAll()
         rotate(60, -60)
     }
@@ -67,31 +63,29 @@ function crossRotate (go: number) {
     paused = false
 }
 
-radio.onReceivedValue(function(name: string, value: number) {
-    if (name == "gocross") 
-    {
+radio.onReceivedValue(function (name: string, value: number) {
+    if (name == "gocross") {
         go = value
-    }
-    if (name == "forcego") {
+    } else if (name == "forcego") {
         crossRotate(go)
     }
 })
 
-function getSenzors () : senzors {
-    let c = (whiteLine ^ pins.digitalReadPin(pinC)) == 0 ? 0 : 1
-    let l = (whiteLine ^ pins.digitalReadPin(pinL)) == 0 ? 0 : 1
-    let r = (whiteLine ^ pins.digitalReadPin(pinR)) == 0 ? 0 : 1
-    return {c,l,r}
+function getSenzors(): senzors {
+    let c = (whiteLine ^ pins.digitalReadPin(pinC)) == 1
+    let l = (whiteLine ^ pins.digitalReadPin(pinL)) == 1
+    let r = (whiteLine ^ pins.digitalReadPin(pinR)) == 1
+    return { c, l, r }
 }
 
 basic.forever(function () {
     if (paused) return
-    if (autoModeEnabled) { 
+    if (autoModeEnabled) {
         let s = getSenzors()
         let c = s.c
         let l = s.l
         let r = s.r
-        console.log([c,r,l])
+        console.log([c, r, l])
         if (!l && !r) {
             forward()
             console.log("center")
@@ -101,9 +95,13 @@ basic.forever(function () {
         } else if (!r) {
             right(_turn)
             console.log("right")
-        } else if (l && r){
+        } else if (l && r) {
             crossRotate(go)
+        } else if (l && c && go == 1) {
+            crossRotate(1)
+        } else if (r && c && go == 2) {
+            crossRotate(2)
         }
-} 
+    }
 
 })
